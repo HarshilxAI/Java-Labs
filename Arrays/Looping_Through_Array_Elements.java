f7c962511c44f28e131d0d4aea51e5e921224cc6
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-11-02 12:44:18 +0530

    Refactor Looping_Through_Array_Elements.java

--- Arrays/Looping_Through_Array_Elements.java
+++ Arrays/Looping_Through_Array_Elements.java
@@ -3,21 +3,25 @@ import java.util.*;
 
 public class Looping_Through_Array_Elements {
     class Main {
+        
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int[] age = new int[3];
 
             System.out.println("Enter 3 ages:");
             for(int i = 0; i < age.length; i++) {
+                
                 age[i] = sc.nextInt();
             }
 
             System.out.println("Using for Loop:");
             for(int i = 0; i < age.length; i++) {
                 System.out.println(age[i]);
+                
             }
 
             sc.close();
         }
     }
 }
+
