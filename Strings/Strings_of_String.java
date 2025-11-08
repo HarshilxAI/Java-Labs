376420aa1ac679bbbdec796d327eba41bea71707
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-11-08 21:45:16 +0530

    Add files via upload

---
+++ Strings/Strings_of_String.java
@@ -0,0 +1,24 @@
+package Strings;
+import java.util.*;
+
+public class Strings_of_String {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+
+
+String extermee = sc.nextLine();
+
+    System.out.println("String: " + extermee);
+
+
+    int length = extermee.length();
+System.out.println("Length: " + length);
+
+    sc.close();
+  } 
+
+
+}
