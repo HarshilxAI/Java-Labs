4e3f4dccae6d77fa19cc4280a990c1e8ccb6d4ef
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2026-01-31 20:54:59 +0530

    Refactor InputOperation.java for clarity

--- Basic_code/InputOperation.java
+++ Basic_code/InputOperation.java
@@ -1,17 +1,19 @@
 import java.util.*;
 class InputOperation {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner (System.in);
+        
         System.out.print("Enter the value of A : ");
         int a = sc.nextInt();
-        System.out.print("Enter the value of B : ");
-        int b = sc.nextInt();
+          System.out.print("Enter the value of B : ");
+           int b = sc.nextInt();
 
 
         int sum = a + b ;
-        int diff = a - b ;
-        int mul = a * b ;
-        int div = a / b ; 
+         int diff = a - b ;
+          int mul = a * b ;
+         int div = a / b ; 
 
         System.out.println("The sum of " + a + " and " + b + " is : " + sum);
         System.out.println("The diff of " + a + " and " + b + " is : " + diff);
@@ -21,3 +23,4 @@ class InputOperation {
         sc.close();
     }
 }
+
